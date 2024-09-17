75bb995334138001aedf34d62d3afb234560170a
Malikd96 <96malikdavis@gmail.com>
2024-09-16 20:32:37 -0400

    To Do List

--- to_do_list_app.py
+++ to_do_list_app.py
@@ -9,16 +9,11 @@
         #4. Delete a task
         #5. Quit
 
-import argparse
-
-
 task = []
 status = []
-
-
 def to_do_list_app():
-   print("Welcome to the To Do List App! ")
-   print("""
+      print("Welcome to the To Do List App! ")
+      print("""
 Menu:
 1. Add a task
 2. View tasks
@@ -54,15 +49,24 @@ def delete_task():
    print("Task has been deleted. ")
 
 while True:
-        to_do_list_app()
-        user = input('Please select a number 1-5: ')
-        if user == '1':
-            add_a_task()
-        elif user == '2':
-            view_tasks()
-        elif user == '3':
-            completed_task()
-        elif user == '4':
-            delete_task()
-        elif user == '5':
-            break
+   to_do_list_app()
+   try:
+
+      user = input('Please select a number 1-5: ')
+      if user.isalpha():
+         raise ValueError
+      if user == '1':
+         add_a_task()
+      elif user == '2':
+         view_tasks()
+      elif user == '3':
+         completed_task()
+      elif user == '4':
+         delete_task()
+      elif user == '5':
+         break
+   except ValueError:
+      print("Invalid input, please try again.")
+
+print('Thanks for trying out the To Do List App!')
+   
